b3df40aa0953ce48188859c99ce5304058bbec7d
Sayem Hossain <sayemkcn@gmail.com>
2019-05-16 18:23:14 +0600

    Fixed PageRequest (Constructor deprecation)

--- src/main/kotlin/com/example/webservice/commons/PageAttr.java
+++ src/main/kotlin/com/example/webservice/commons/PageAttr.java
@@ -9,11 +9,11 @@ public class PageAttr {
     public static String SORT_BY_FIELD_ID = "id";
 
     public static PageRequest getPageRequest(int page){
-        return new PageRequest(page, PageAttr.PAGE_SIZE, Sort.Direction.DESC,PageAttr.SORT_BY_FIELD_ID);
+        return PageRequest.of(page, PageAttr.PAGE_SIZE, Sort.Direction.DESC,PageAttr.SORT_BY_FIELD_ID);
     }
 
     public static PageRequest getPageRequest(int page, int size) {
         if (size <= 0) size = 10;
-        return new PageRequest(page, size, Sort.Direction.DESC, PageAttr.SORT_BY_FIELD_ID);
+        return PageRequest.of(page, size, Sort.Direction.DESC, PageAttr.SORT_BY_FIELD_ID);
     }
 }
