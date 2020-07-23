826db6189e65933ea4a7093e71ab54621157c8a9
Sayem Hossain <sayemkcn@gmail.com>
2020-07-23 15:31:25 +0600

    [Add] example module

---
+++ examples/examplemodule/src/test/java/com/example/example/ExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
