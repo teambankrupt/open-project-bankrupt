d517ca7e85562fb6149de4cc24378d589ec2512a
Sayem Hossain <sayemkcn@gmail.com>
2020-08-21 22:52:34 +0600

    [Remove] Goal and Issue CRUD that was added for testing

--- app/src/main/kotlin/com/example/app/domains/issues/services/IssueService.kt
+++
@@ -1,6 +0,0 @@
-package com.example.app.domains.issues.services
-
-import com.example.app.domains.issues.models.entities.Issue
-import com.example.coreweb.domains.base.services.CrudService
-
-interface IssueService : CrudService<Issue>
