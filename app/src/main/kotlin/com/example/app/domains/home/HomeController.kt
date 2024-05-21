9a3a85f9f75932e40a64e17705364affb33c3afe
Sayem Hossain <mail@sayem.dev>
2024-05-21 16:00:37 +0600

    Upgraded modules

--- app/src/main/kotlin/com/example/app/domains/home/HomeController.kt
+++ app/src/main/kotlin/com/example/app/domains/home/HomeController.kt
@@ -1,10 +1,9 @@
 package com.example.app.domains.home
 
-import com.example.app.MainApplication
-import com.example.app.routing.Route
+import com.example.auth.config.security.SecurityContext
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
 import springfox.documentation.annotations.ApiIgnore
 
 @Controller
@@ -12,18 +11,10 @@ import springfox.documentation.annotations.ApiIgnore
 class HomeController {
 
     @GetMapping("")
-    fun home(): String {
+    fun home(model: Model): String {
+        val auth = SecurityContext.getCurrentUser()
+        model.addAttribute("auth", auth)
         return "index"
     }
 
-    @PostMapping(Route.V1.WEB_RELOAD_APPLICATION_CONTEXT)
-    fun reloadApplicationContext(): String {
-        MainApplication.restart()
-        return "redirect:${com.example.acl.routing.Route.V1.WEB_ROLES_PAGE}"
-    }
-
-    @PostMapping(Route.V1.WEB_SHUTDOWN_APPLICATION_CONTEXT)
-    fun closeApplicationContext() {
-        MainApplication.terminate()
-    }
 }
